c311e5cf47af6edc4b0cd54c59eacb41de44bbe4
kk366-code <kkatayama366@gmail.com>
2025-12-13 21:07:42 +0900

    update echo

--- chapter05/example-5-10-echo1.c
+++ chapter05/example-5-10-echo1.c
@@ -6,6 +6,14 @@
 
 #include <stdio.h>
 
+// char *argv[] は、char * 型（文字へのポインタ、つまり文字列）の配列を意味する。
+// C言語では、配列を関数に渡す際、それは配列の先頭要素へのポインタとして渡される。
+// argv の実際の型は char **（char へのポインタへのポインタ）。
+
+// char	1文字
+// char *	文字列（char へのポインタ）
+// char ** (または char *[])	文字列へのポインタの配列（ポインタのポインタ）
+
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
