5cd97657455610f9e419920474cee44a082107b4
kk366-code <kkatayama366@gmail.com>
2025-12-13 20:58:06 +0900

    update echo

--- chapter05/example-5-10-echo1.c
+++ chapter05/example-5-10-echo1.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
     }
     printf("\n");
